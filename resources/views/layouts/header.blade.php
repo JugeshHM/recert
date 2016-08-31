306347f8bb778ab7a7fa365a938b4c7ef6430257
smartameer <smartameer@icloud.com>
2016-08-31 19:13:54 +0530

    Updating layouts

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,8 +12,12 @@
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
+        @if (Auth::check())
+        <li><a href="{{URL::to('/auth/logout')}}">Logout</a></li>
+        @else
         <li><a href="{{URL::to('/auth/login')}}">Login</a></li>
         <li><a href="{{URL::to('/auth/register')}}">Register</a></li>
+        @endif
       </ul>
     </div>
   </div>
